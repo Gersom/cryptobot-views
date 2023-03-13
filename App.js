d8377c44e4da2fa458ee616504ba8e8068a5ed01
Gersom Lap <gersomalaja@gmail.com>
2023-03-13 17:04:28 -0500

    Add Screen Config

--- App.js
+++ App.js
@@ -7,12 +7,13 @@ import Register from './src/screens/Register';
 import Home from './src/screens/Home';
 import Conexion from './src/screens/Conexion';
 import List from './src/screens/List';
+import Config from './src/screens/Config';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <List />
+      <Config />
     </View>
   );
 }
